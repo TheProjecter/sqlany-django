355a08bc8b0970cdd4cf439421ef881b9bb9532c
Graeme Perrow <graeme.perrow@sap.com>
2013-06-27 16:07:07 -0400

    Submission of v1.4 of the SQL Anywhere Django driver

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sqlany_django',
+      version='1.4',
+      description='SQL Anywhere database backend for Django',
+      author='Graeme Perrow',
+      author_email='graeme.perrow@sap.com',
+      url='http://code.google.com/p/sqlany-django',
+      packages=['sqlany_django'],
+      )
