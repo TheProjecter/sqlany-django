6b72a9539aeac79318057185488f50da3fb077b5
Graeme Perrow <graeme.perrow@sap.com>
2013-11-13 15:02:48 -0500

    Added ability to use PyPI to install sqlany_django

--- setup.py
+++ setup.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env python
+# ***************************************************************************
+# Copyright (c) 2013 SAP AG or an SAP affiliate company. All rights reserved.
+# ***************************************************************************
 
-from distutils.core import setup
+r"""sqlany-django - SQL Anywhere driver for Django.
+
+http://code.google.com/p/sqlany-django/
+
+----------------------------------------------------------------"""
+
+from setuptools import setup, find_packages
 
 setup(name='sqlany_django',
       version='1.4',
       description='SQL Anywhere database backend for Django',
+      long_description=open('README.rst').read(),
       author='Graeme Perrow',
       author_email='graeme.perrow@sap.com',
       url='http://code.google.com/p/sqlany-django',
-      packages=['sqlany_django'],
+      packages = find_packages(),
+      license='New BSD',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
       )
